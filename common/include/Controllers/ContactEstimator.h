ce317c43621a0552cb6a6609c278d7c7cc406f69
He Li <bbeiter1@nd.edu>
2020-10-02 14:58:36 -0400

    Add foot contact detection

--- common/include/Controllers/ContactEstimator.h
+++ common/include/Controllers/ContactEstimator.h
@@ -12,27 +12,54 @@
 #define PROJECT_CONTACTESTIMATOR_H
 
 #include "Controllers/StateEstimatorContainer.h"
+#include "Controllers/LegController.h"
+#include "Dynamics/FloatingBaseModel.h"
+#include "Dynamics/Quadruped.h"
+
 
 /*!
  * A "passthrough" contact estimator which returns the expected contact state
  */
 template <typename T>
-class ContactEstimator : public GenericEstimator<T> {
+class ContactEstimator : public GenericEstimator<T>{
  public:
+  ContactEstimator():GenericEstimator<T>(){
+    _state.bodyVelocity = SVec<T>::Zero();
+    _state.bodyPosition = Vec3<T>::Zero();
+    _state.bodyOrientation = Quat<T>::Zero();
+    _state.q = DVec<T>::Zero(12);
+    _state.qd = DVec<T>::Zero(12);
+  }
+ 
 
   /*!
    * Set the estimated contact by copying the exptected contact state into the
    * estimated contact state
    */
-  virtual void run() {
-    this->_stateEstimatorData.result->contactEstimate =
-        *this->_stateEstimatorData.contactPhase;
-  }
+  virtual void run();
+
+  // virtual void run() {
+
+  //   this->_stateEstimatorData.result->contactEstimate =
+  //       *this->_stateEstimatorData.contactPhase;
+  // }
 
   /*!
    * Set up the contact estimator
    */
-  virtual void setup() {}
+  virtual void setup();
+
+  private:
+  FBModelState<T> _state; 
+  Quadruped<T> *_robot;
+  LegControllerData<T> *_legDatas;
+  std::vector<Vec3<T>> world_foot_loc_old;
+  std::vector<Vec3<T>> world_foot_loc_new;
+  std::vector<Vec3<T>> world_foot_vel;
+  size_t _foot_ID[4]; // FR FL HR HL
+  T _diff[4];
+  Vec3<T> _loc_foot;
+
 };
 
 #endif  // PROJECT_CONTACTESTIMATOR_H
